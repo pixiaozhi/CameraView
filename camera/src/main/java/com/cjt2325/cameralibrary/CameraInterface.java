289e4ebf3df7134311da31bd6e2999d3434263df
Irfan Raza <irfaan.aa@gmail.com>
2017-10-10 20:39:13 +0530

    set camera front

--- camera/src/main/java/com/cjt2325/cameralibrary/CameraInterface.java
+++ camera/src/main/java/com/cjt2325/cameralibrary/CameraInterface.java
@@ -286,7 +286,7 @@ public class CameraInterface implements Camera.PreviewCallback {
 
     private CameraInterface() {
         findAvailableCameras();
-        SELECTED_CAMERA = CAMERA_POST_POSITION;
+        SELECTED_CAMERA = CAMERA_FRONT_POSITION;
         saveVideoPath = "";
     }
 
